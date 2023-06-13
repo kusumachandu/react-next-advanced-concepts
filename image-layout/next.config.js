bae7412a08ed3f2f512f138a28b4547b66c3b10a
chandrashekar <kusumachandu43@gmail.com>
2023-06-13 15:46:59 +0530

    config file changed

--- image-layout/next.config.js
+++ image-layout/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'iso.500px.com',
+        // You can add these as well
+        // port: '',
+        // pathname: 'arifscloud/image/upload/**',
+      },
+    ],
+  },
+
 }
 
 module.exports = nextConfig
